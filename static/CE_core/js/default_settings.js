792a25658f13628df3e06a8f679908c2abf8c498
cat <910384+catsmith@users.noreply.github.com>
2019-03-27 17:29:27 +0000

    restructuring to allow for easier reading

--- static/CE_core/js/default_settings.js
+++ static/CE_core/js/default_settings.js
@@ -62,25 +62,27 @@ var DEF = (function () {
               {
                   "id": "ignore_supplied",
                   "label": "Ignore supplied markers",
-                  "linked_to_settings": true,
-                  "setting_id": "view_supplied",
                   "function": "ignore_supplied",
                   "apply_when": true,
                   "check_by_default": false,
                   "type": "string_application"
+                  "linked_to_settings": true,
+                  "setting_id": "view_supplied",
+
               },
               {
                   "id": "ignore_unclear",
                   "label": "Ignore unclear markers",
-                  "linked_to_settings": true,
-                  "setting_id": "view_unclear",
                   "function": "ignore_unclear",
                   "apply_when": true,
                   "check_by_default": false,
                   "type": "string_application"
+                  "linked_to_settings": true,
+                  "setting_id": "view_unclear",
+
               }
           ]
-      }
+      },
 
     	displaySettings:
     		{
@@ -88,36 +90,35 @@ var DEF = (function () {
     			"class_name": "ApplySettings",
     			"configs":
     			[
-
-    			      {
-    			    	  	"id": "view_supplied",
-                            "label": "view supplied text",
-                            "function": "hide_supplied_text",
-                            "menu_pos": 1,
-                            "execution_pos": 2,
-                            "check_by_default": true,
-                            "apply_when": false
-    			      },
-    			      {
-                            "id": "view_unclear",
-                            "label": "view unclear text",
-                            "function": "hide_unclear_text",
-                            "menu_pos": 2,
-                            "execution_pos": 3,
-                            "check_by_default": true,
-                            "apply_when": false
-    			      },
-                      {
-                            "id": "view_capitalisation",
-                            "label": "view capitalisation",
-                            "function": "lower_case",
-                            "menu_pos": 3,
-                            "execution_pos": 1,
-                            "check_by_default": false,
-                            "apply_when": false
-                       }
+  			      {
+  			    	  	"id": "view_supplied",
+                  "label": "view supplied text",
+                  "function": "hide_supplied_text",
+                  "apply_when": false,
+                  "check_by_default": true,
+                  "menu_pos": 1,
+                  "execution_pos": 2
+  			      },
+  			      {
+                  "id": "view_unclear",
+                  "label": "view unclear text",
+                  "function": "hide_unclear_text",
+                  "apply_when": false,
+                  "check_by_default": true,
+                  "menu_pos": 2,
+                  "execution_pos": 3
+  			      },
+              {
+                  "id": "view_capitalisation",
+                  "label": "view capitalisation",
+                  "function": "lower_case",
+                  "apply_when": false,
+                  "check_by_default": false,
+                  "menu_pos": 3,
+                  "execution_pos": 1
+               }
     			  ]
-            },
+          },
 
 
 
