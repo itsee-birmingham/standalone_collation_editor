d892fce1091ae1d835b744e57a17a1076e747dd4
cat <910384+catsmith@users.noreply.github.com>
2021-09-14 18:01:03 +0100

    more comments for debugging

--- static/CE_core/js/set_variants.js
+++ static/CE_core/js/set_variants.js
@@ -4795,7 +4795,7 @@ SV = (function() {
         unit = CL.data[appId][unitPos];
         reading = unit.readings[readingPos];
         console.log('_addEvent')
-        console.log(reading)
+        console.log(JSON.parse(JSON.stringify(reading)))
         readingDetails = {
           'app_id': appId,
           'unit_id': unit._id,
