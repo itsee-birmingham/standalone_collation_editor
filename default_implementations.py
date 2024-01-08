69295af6844c9512e42d87fd49f2138e68ba11df
Cat Smith <910384+catsmith@users.noreply.github.com>
2024-01-08 17:23:58 +0000

    linting

--- default_implementations.py
+++ default_implementations.py
@@ -6,12 +6,12 @@ class RuleConditions(object):
     def ignore_unclear(self, decision_word, token_words):
         decision_word = decision_word.replace('_', '')
         token_words = [w.replace('_', '') for w in token_words]
-        return(decision_word, token_words)
+        return (decision_word, token_words)
 
     def ignore_supplied(self, decision_word, token_words):
         decision_word = decision_word.replace('[', '').replace(']', '')
         token_words = [w.replace('[', '').replace(']', '') for w in token_words]
-        return(decision_word, token_words)
+        return (decision_word, token_words)
 
 
 class ApplySettings(object):
